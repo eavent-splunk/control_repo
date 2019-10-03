add50266d903785d6c07bd4a8d3436cf438fe690
Edward Avent <eavent@splunk.com>
2019-10-03 17:13:25 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,10 @@ node default {
 # This node name might need to be eaventlin03.sv.splunk.com...
 node 'eaventlin03.sv.splunk.com' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+  }
 }
 node /^web/ {
  include role::app_server
